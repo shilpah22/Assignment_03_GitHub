5e69d0d2d70fdd0f9795e929cebc09c3104970a4
shilpah22 <havalada.shilpa@gmail.com>
2025-06-04 21:54:52 +0100

    Update test_file1.py

--- test_file.py
+++ test_file.py
@@ -1,1 +1,4 @@
 print("Your name with your college ID below this message")
+name=input("Enter your name:")
+id=input("Enter your college id:")
+print("Student name is {} and id is {}".format(name,id))
